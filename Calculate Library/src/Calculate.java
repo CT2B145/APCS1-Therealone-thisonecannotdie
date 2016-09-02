3ca8a627367ab52e6ecd915c0fff55d9083e3dae
CT2B145 <mjpon45@gmail.com>
2016-09-01 21:41:52 -0700

    Nearly done wiht part one

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -34,12 +34,55 @@ public class Calculate {
 		double answerofa = (b*b)-4*a*c;
 		return answerofa;
 	
-	}public static int toImproperFrac(int cookie, int are, int cool){
+	}public static String toImproperFrac(int cookie, int are, int cool){
 		
-		ok = cookie
-		String 
+		int addthemup = (cookie*cool)+are;
+		int thefraction = addthemup/2;
+		String numberAsString = Integer.toString(thefraction);
+		return numberAsString;
+	
+	}public static String MixedNum (int inputter, int inputterrr ){
+		int cookie = inputter/inputterrr;
+		String number1 = Integer.toString(cookie);
+		return number1;
+	}public static String foil (int a, int b, int c, String n){
+		String foils = a+n+"^2 + "+b+n+" - "+c;
+		return foils;
 		
+	}public static boolean isDivisibleBy( int DivInput, int Divinputt){
+		int number = DivInput/Divinputt;
+		int number1 = Divinputt/DivInput;
+		int newnumb =  number1+number;
+		
+	if (newnumb  == 0){
+		return true;
+		}else{
+		return false;
+		
+		}
 	
 	}
+
+	public static double absValue(double number){
+		if(number<0){
+		double number1 = number * -1;
+		return number1;
+	}else{
+		return number;
+		
+		
+		}
+	}
+	public static boolean max(double Trump, double HRC){	
+		if(Trump>HRC){
+			System.out.println("Trump is greater!!!");
+		return true;
+		}else{
+			System.out.println("Trump is still great, despite he is less!");
+			return false;
+		}
+		}
+	}
 	
-}
+		
+		
